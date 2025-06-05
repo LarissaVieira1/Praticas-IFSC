b1ac7ca76309f032047cbbe7bde23dee73b2105d
Larissa <Larissa.vsl@aluno.ifsc.edu.br>
2025-06-05 11:32:49 -0300

    Segundo passo concluído

---
+++ Avaliação 20-05/PDS2-I6-25-1-main/LojaMVC/src/main/java/model/Cliente.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.sql.Date;
+
+public class Cliente {
+    private int idCliente;
+    private String nome;
+    private String telefone;
+    private String endereco;
+    private Date dataNascimento; 
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
+    
+}
